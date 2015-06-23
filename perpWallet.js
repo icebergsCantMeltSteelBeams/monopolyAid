9e25c9d7499e8fa107dee1c85d42f5bd1b9db78c
icebergsCantMeltSteelBeams <jeanclaude.zarate@baxter-academy.org>
2015-06-22 21:46:17 -0400

    IC102

--- perpWallet.js
+++ perpWallet.js
@@ -3,8 +3,8 @@ var perpWallet = function () {
 		document.getElementById("player1BalanceDisplay").innerHTML = players.player1.wallet.fancyBalance();
 		document.getElementById("player2BalanceDisplay").innerHTML = players.player2.wallet.fancyBalance();
 		document.getElementById("player3BalanceDisplay").innerHTML = players.player3.wallet.fancyBalance();
-		players.player1.wallet.balance += +1000;
-		players.player2.wallet.balance += +1000;
-		players.player3.wallet.balance += +1000;
+		//players.player1.wallet.balance += +1000;
+		//players.player2.wallet.balance += +1000;
+		//players.player3.wallet.balance += +1000;
 	 }, 500);
 }
